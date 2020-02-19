ea0a34d12061728f5a51531ca2e32a0e118ddccf
Amador Cuenca <sphi02ac@gmail.com>
2020-02-19 17:55:50 -0300

    feat: Complete the search artist route

--- app.py
+++ app.py
@@ -180,17 +180,14 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-  # search for "band" should return "The Wild Sax Band".
+  search_term = request.form.get('search_term', '')
+  matches = db.session.query(Artist).filter(Artist.name.like(f"%{search_term}%")).all()
+  
   response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
+    "count": len(matches),
+    "data": matches
   }
+  
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/artists/<int:artist_id>')
