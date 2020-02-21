f749b5765caaf88d4b33b7a11564ac0b130b00df
Amador Cuenca <sphi02ac@gmail.com>
2020-02-21 18:32:25 -0300

    feat: Complete the show creation route

--- app.py
+++ app.py
@@ -155,7 +155,6 @@ def create_venue_submission():
     db.session.commit()
     flash('Venue ' + form.name.data + ' was successfully listed!')
   else:
-    print(form.errors)
     flash('An error occurred. Venue ' + form.name.data + ' could not be listed.')
 
   return render_template('pages/home.html')
@@ -331,14 +330,21 @@ def create_shows():
 
 @app.route('/shows/create', methods=['POST'])
 def create_show_submission():
-  # called to create new shows in the db, upon submitting new show listing form
-  # TODO: insert form data as a new Show record in the db, instead
+  form = ShowForm()
+  
+  if form.validate_on_submit():
+    artist = Artist.query.get(form.artist_id.data)
+    venue = Venue.query.get(form.venue_id.data)
+    
+    if not artist or not venue:
+      flash('An error occurred. Show could not be listed.')
+    else:
+      show = Show(artist_id=form.artist_id.data, venue_id=form.venue_id.data,
+                  start_time=form.start_time.data)
+      
+      db.session.add(show)
+      db.session.commit()
 
-  # on successful db insert, flash success
-  flash('Show was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Show could not be listed.')
-  # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
   return render_template('pages/home.html')
 
 @app.errorhandler(404)
