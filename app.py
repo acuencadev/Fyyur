4909c61f39d22ab6efc10e6fb8b17a1c5648baa4
Amador Cuenca <sphi02ac@gmail.com>
2020-02-21 13:22:20 -0300

    feat: Complete the edit venue route

--- app.py
+++ app.py
@@ -243,29 +243,41 @@ def edit_artist_submission(artist_id):
 
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
-  form = VenueForm()
-  venue={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-  }
-  # TODO: populate form with values from venue with ID <venue_id>
+  venue = Venue.query.get(venue_id)
+  
+  if not venue:
+    abort(404)
+  
+  form = VenueForm(name=venue.name, city=venue.city, state=venue.state,
+                   address=venue.address, phone=venue.phone,
+                   facebook_link=venue.facebook_link)
+
   return render_template('forms/edit_venue.html', form=form, venue=venue)
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
 def edit_venue_submission(venue_id):
-  # TODO: take values from the form submitted, and update existing
-  # venue record with ID <venue_id> using the new attributes
-  return redirect(url_for('show_venue', venue_id=venue_id))
+  venue = Venue.query.get(venue_id)
+  
+  if not venue:
+    abort(404)
+  
+  form = VenueForm()
+  
+  if form.validate_on_submit():
+    venue.name = form.name.data
+    venue.city = form.city.data
+    venue.state = form.state.data
+    venue.phone = form.phone.data
+    venue.address = form.address.data
+    venue.image_link = form.image_link.data
+    venue.facebook_link = form.facebook_link.data
+    
+    db.session.commit()
+  
+    return redirect(url_for('show_venue', venue_id=venue_id))
+  else:
+    print(form.errors)
+    return redirect(url_for('edit_venue', venue_id=venue_id))
 
 #  Create Artist
 #  ----------------------------------------------------------------
