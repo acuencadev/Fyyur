0ecc8f9eb5359fa5d03e53d6f29ae3ca404e56b5
Amador Cuenca <sphi02ac@gmail.com>
2020-02-21 14:08:56 -0300

    feat: Complete the create artist route

--- app.py
+++ app.py
@@ -289,16 +289,20 @@ def create_artist_form():
 
 @app.route('/artists/create', methods=['POST'])
 def create_artist_submission():
-  # called upon submitting the new artist listing form
-  # TODO: insert form data as a new Venue record in the db, instead
-  # TODO: modify data to be the data object returned from db insertion
-
-  # on successful db insert, flash success
-  flash('Artist ' + request.form['name'] + ' was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
+  form = ArtistForm()
+  artist = Artist(name=form.name.data, city=form.city.data, state=form.state.data,
+                  phone=form.phone.data, genres=form.genres.data,
+                  image_link=form.image_link.data, facebook_link=form.facebook_link.data)
+  
+  if form.validate_on_submit():
+    db.session.add(artist)
+    db.session.commit()
+    
+    flash('Artist ' + form.name.data + ' was successfully listed!')
+  else:
+    flash('An error occurred. Artist ' + artist.name + ' could not be listed.')
+    
   return render_template('pages/home.html')
-
 
 #  Shows
 #  ----------------------------------------------------------------
