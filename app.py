d515bd0beac8fae3d68f9a89e3cae63e95a7f170
Amador Cuenca <sphi02ac@gmail.com>
2020-02-21 14:44:11 -0300

    feat: Complete the edit artist route

--- app.py
+++ app.py
@@ -197,29 +197,40 @@ def show_artist(artist_id):
 #  ----------------------------------------------------------------
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
-  form = ArtistForm()
-  artist={
-    "id": 4,
-    "name": "Guns N Petals",
-    "genres": ["Rock n Roll"],
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "326-123-5000",
-    "website": "https://www.gunsnpetalsband.com",
-    "facebook_link": "https://www.facebook.com/GunsNPetals",
-    "seeking_venue": True,
-    "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-    "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-  }
-  # TODO: populate form with fields from artist with ID <artist_id>
+  artist = Artist.query.get(artist_id)
+  
+  if not artist:
+    abort(404)
+  
+  form = ArtistForm(name=artist.name, city=artist.city, state=artist.state,
+                    phone=artist.phone, genres=artist.genres,
+                    image_link=artist.image_link, facebook_link=artist.facebook_link)
+  
   return render_template('forms/edit_artist.html', form=form, artist=artist)
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
-  # TODO: take values from the form submitted, and update existing
-  # artist record with ID <artist_id> using the new attributes
-
-  return redirect(url_for('show_artist', artist_id=artist_id))
+  artist = Artist.query.get(artist_id)
+  
+  if not artist:
+    abort(404)
+    
+  form = ArtistForm()
+  
+  if form.validate_on_submit():
+    artist.name = form.name.data
+    artist.city = form.city.data
+    artist.state = form.state.data
+    artist.phone = form.phone.data
+    artist.genres = form.genres.data
+    artist.image_link = form.image_link.data
+    artist.facebook_link = form.facebook_link.data
+  
+    db.session.commit()
+    
+    return redirect(url_for('show_artist', artist_id=artist_id))
+  else:
+    return redirect(url_for('edit_artist', artist_id=artist_id))
 
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
